2c35033af98603616dbee75880280d1c51d29b27
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-05-26 12:16:19 +0700

    63. Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { publicRoutes } from '@/routes';
+import { DefaultLayout } from '@/components/Layout';
+import { Fragment } from 'react';
+
 function App() {
+    const renderPublicRoutes = () => {
+        return publicRoutes.map((route, index) => {
+            const { path, component: Page } = route;
+            // const Layout = route.layout === null ? Fragment : DefaultLayout;
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+                Layout = route.layout;
+            } else if (route.layout === null) {
+                Layout = Fragment;
+            }
+
+            return (
+                <Route
+                    key={index}
+                    path={path}
+                    element={
+                        <Layout>
+                            <Page />
+                        </Layout>
+                    }
+                />
+            );
+        });
+    };
+
     return (
-        <div className="App">
-            <h1>learn react </h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>{renderPublicRoutes()}</Routes>
+            </div>
+        </Router>
     );
 }
 
