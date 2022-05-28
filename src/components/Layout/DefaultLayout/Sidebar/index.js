93b74093949db223f7d675c1223b539c86d0e7a9
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-05-28 22:31:14 +0700

    64. Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Sidebar = () => {
-    return <h2>Sidebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 };
 
 export default Sidebar;
